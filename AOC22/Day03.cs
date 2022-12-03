90903d238fddb99ae222d3cb872ce30be1d7fa65
Pascal Hauser <pascal.hauser@additiv.com>
2022-12-03 10:37:16 +0100

    day03 done

---
+++ AOC22/Day03.cs
@@ -0,0 +1,37 @@
+﻿
+namespace AOC22
+{
+    internal class Day03 : BaseDay
+    {
+        protected override void DoRunPart01()
+        {
+            var sum = 0;
+            foreach (var line in InputData)
+            {
+                var str1 = line[..(line.Length / 2)];
+                var str2 = line[(line.Length / 2)..];
+                var common = str1.Intersect(str2).FirstOrDefault();
+
+                //lowercase -96
+                //uppercase -38
+                sum += common > 90 ? common - 96 : common - 38;
+
+            }
+
+            Console.WriteLine($"Solution Part01: {sum}");
+        }
+
+        protected override void DoRunPart02()
+        {
+            var sum = 0;
+            for (var i = 0; i < InputData.Count; i+=3)
+            {
+                var commonCharsForTwoLines = InputData[i].Intersect(InputData[i+1]).ToList();
+                var commonChar = commonCharsForTwoLines.Intersect(InputData[i+2]).FirstOrDefault();
+                sum += commonChar > 90 ? commonChar - 96 : commonChar - 38;
+            }
+
+            Console.WriteLine($"Solution Part02: {sum}");
+        }
+    }
+}
--- AOC22/Program.cs
+++ AOC22/Program.cs
@@ -1,9 +1,13 @@
 ﻿using AOC22;
 
-var day01 = new Day01();
-day01.RunPart01();
-day01.RunPart02();
+//var day01 = new Day01();
+//day01.RunPart01();
+//day01.RunPart02();
 
-var day02 = new Day02();
-day02.RunPart01();
-day02.RunPart02();
+//var day02 = new Day02();
+//day02.RunPart01();
+//day02.RunPart02();
+
+var day03 = new Day03();
+day03.RunPart01();
+day03.RunPart02();
